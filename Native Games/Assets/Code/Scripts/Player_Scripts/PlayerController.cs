c7c32372ce9a3f3fa423bb6e4016a8367ba696e7
Davi Fonseca Darienzo <davireshiram3@hotmail.com>
2025-09-12 18:34:41 -0300

    Player se move em rel. a Cam e recebe Input

--- Native Games/Assets/Code/Scripts/Player_Scripts/PlayerController.cs
+++ Native Games/Assets/Code/Scripts/Player_Scripts/PlayerController.cs
@@ -1,15 +1,26 @@
 using KBCore.Refs;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class PlayerController : ValidatedMonoBehaviour
 {
     [SerializeField, Self] CharacterController characterController;
+    [SerializeField, Scene] CinemachineCamera cinemachineCamera;
 
     [SerializeField, Range(0.0f, 100.0f)] float moveSpeed = 3.5f;
 
     private void Update()
     {
-        Vector3 motionDisplacement = transform.forward * moveSpeed * Time.deltaTime;
-        characterController.Move(motionDisplacement);
+        Vector2 motionInput = InputManager.Instance.MotionInput;
+        Vector3 motionVector = new Vector3(motionInput.x, 0.0f, motionInput.y);
+        
+        Vector3 cameraForwardDirection = cinemachineCamera.transform.forward;
+        cameraForwardDirection.y = 0.0f;
+        cameraForwardDirection.Normalize();
+
+        Quaternion cameraForwardRotation = Quaternion.LookRotation(cameraForwardDirection);
+
+        Vector3 relativeMotionVector = cameraForwardRotation * motionVector;
+        characterController.Move(relativeMotionVector * moveSpeed * Time.deltaTime);
     }
 }
