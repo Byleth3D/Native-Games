31522909b3f6590661c055c6811f9d12724a6878
Davi Fonseca Darienzo <davireshiram3@hotmail.com>
2025-10-25 14:28:31 -0300

    O Carlinhos bateu o record mundial

--- Native Games/Assets/Code/Scripts/Misc/CheckpointCutsceneController.cs
+++ Native Games/Assets/Code/Scripts/Misc/CheckpointCutsceneController.cs
@@ -1,16 +1,23 @@
 using EditorAttributes;
 using UnityEngine;
 
-public class CheckpointTieCutsceneController : MonoBehaviour
+public class CheckpointCutsceneController : MonoBehaviour
 {
     [SerializeField] private int checkpointIndex = -1;
 
-    private void Start()
+    private void Update()
     {
         if (checkpointIndex > -1)
         {
             int currentSaveIndex = SaveManager.Instance.CurrentSaveIndex;
+
+            if (currentSaveIndex == -1)
+            {
+                return;
+            }
+
             int currentCheckpointIndex = SaveManager.Instance.SaveFiles[currentSaveIndex].checkpointIndex;
+
             if (currentCheckpointIndex != checkpointIndex)
             {
                 this.gameObject.SetActive(false);
