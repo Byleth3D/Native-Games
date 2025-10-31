162eff0133eb83667f5d519a849248f633d4a7ff
Davi Fonseca Darienzo <davireshiram3@hotmail.com>
2025-10-31 19:48:52 -0300

    Correções InEditorStarter

--- Native Games/Assets/Code/Scripts/GameStarter.cs
+++ Native Games/Assets/Code/Scripts/GameStarter.cs
@@ -1,25 +1,8 @@
-﻿using UnityEditor;
-using UnityEditor.SceneManagement;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 
-[InitializeOnLoad]
+
 public static class GameStarter
 {
-    static GameStarter()
-    {
-        EditorApplication.playModeStateChanged += SaveActiveEditorScene;
-    }
-
-    public static void SaveActiveEditorScene(PlayModeStateChange playeModeState)
-    {
-        if (playeModeState == PlayModeStateChange.ExitingEditMode)
-        {
-            string activeSceneName = EditorSceneManager.GetActiveScene().name;
-            PlayerPrefs.SetString("ActiveEditorScene", activeSceneName);
-            PlayerPrefs.Save();
-        }
-    }
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     public static void SetupGame()
@@ -36,7 +19,7 @@ public static class GameStarter
             QualitySettings.vSyncCount = 1;
         }
 #else
-        SceneManager.LoadSceneAsync("GlobalManagers");
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("GlobalManagers");
 #endif
     }
 }
---
+++ Native Games/Assets/Code/Scripts/InEditorStarter.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class InEditorStarter
+{
+    static InEditorStarter()
+    {
+        EditorApplication.playModeStateChanged += SaveActiveEditorScene;
+    }
+
+    public static void SaveActiveEditorScene(PlayModeStateChange playeModeState)
+    {
+        if (playeModeState == PlayModeStateChange.ExitingEditMode)
+        {
+            string activeSceneName = EditorSceneManager.GetActiveScene().name;
+            PlayerPrefs.SetString("ActiveEditorScene", activeSceneName);
+            PlayerPrefs.Save();
+        }
+    }
+}
+#endif
